b969602978e004f6fa8c5e97e09a6bbe3a5441db
Patrick Cook <qphysicscook@gmail.com>
2023-12-21 17:13:08 -0500

    verified that upcasting works natively for arrays

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ mod ad_ndarray;
 #[cfg(feature = "ndarray")]
 use crate::ad_ndarray::impls::*;
 #[cfg(feature = "ndarray")]
-use ndarray::{Array, Array1, Array2, Dimension};
+use ndarray::{Array, Array1, Array2, Array3, Dimension};
 
 #[cfg(feature = "complex")]
 mod complex;
@@ -30,6 +30,39 @@ use std::marker::PhantomData;
 use std::ops::{Add, Div, Mul, Neg, Sub};
 
 // ================================================= //
+
+#[derive(Debug, Clone, Copy)]
+struct Upcast();
+// takes in an Array1<f64> and returns an Array2<f64> where each row is the input
+
+impl Upcast {
+    fn new() -> Self {
+        Self()
+    }
+}
+
+impl<'a> AutoDiffable<'a, usize, Array1<f64>, Array2<f64>, Array3<f64>> for Upcast {
+    fn eval(&self, x: &Array1<f64>, rows: &usize) -> Array2<f64> {
+        let mut out = Array2::<f64>::zeros((*rows, x.len()));
+        for i in 0..*rows {
+            out.row_mut(i).assign(x);
+        }
+        out
+    }
+
+    /// grad[i, j, k] = d out[j, k] / d x[i]
+    fn grad(&self, x: &Array1<f64>, rows: &usize) -> Array3<f64> {
+        let mut g = Array3::<f64>::zeros((x.len(), *rows, x.len()));
+        for j in 0..*rows {
+            for i in 0..x.len() {
+                g[[i, j, i]] = 1.0;
+            }
+        }
+        g
+    }
+
+}
+
 
 #[derive(Debug, Clone)]
 struct Exp<A, D>(PhantomData<(A, D)>)
@@ -67,15 +100,16 @@ where
 }
 
 fn main() {
-    let x = Array1::<i64>::from(vec![1, 2, 3]);
+    let x = Array1::<f64>::from(vec![1.0, 2.0, 3.0]);
+    let rows = 2;
 
-    let f = AutoDiff::new(Identity::new());
-    println!("f(x) = {}", f.eval(&x, &()));
-    println!("f'(x) = {}", f.grad(&x, &()));
+    let f = AutoDiff::new(Upcast::new());
+    println!("f(x) = {}", f.eval(&x, &rows));
+    println!("f'(x) = {}", f.grad(&x, &rows));
 
-    let g = (f * Array1::from(vec![2]))/(f*f);
-    println!("g(x) = {}", g.eval(&x, &()));
-    println!("g'(x) = {}", g.grad(&x, &()));
+    let g = f * f * f + f;
+    println!("g(x) = {}", g.eval(&x, &rows));
+    println!("g'(x) = {}", g.grad(&x, &rows));
 
     //let c = AutoDiff::new(Constant::new(x.clone()));
 
